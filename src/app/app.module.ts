b4a07e469a0353baa8d3b71ab1dc44bacdf475d8
admin <admin@admins-MacBook-Pro.local>
2017-09-28 00:25:13 +0100

    fixes routing issue by adding correct injection values and changing the Angular router version to match Angular 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {RouterModule, Routes} from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule} from 'angularfire2';
+import { FirebaseService } from './services/firebase.service';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ListingsComponent } from './components/listings/listings.component';
@@ -12,9 +13,19 @@ import { ListingComponent } from './components/listing/listing.component';
 import { AddListingComponent } from './components/add-listing/add-listing.component';
 import { EditListingComponent } from './components/edit-listing/edit-listing.component';
 
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDBJhQcXoiCnCUvpYeTDtHqJSV9sVjMN3w',
+    authDomain: 'address-book-a27b4.firebaseapp.com',
+    databaseURL: 'https://address-book-a27b4.firebaseio.com',
+    projectId: 'address-book-a27b4',
+    storageBucket: 'address-book-a27b4.appspot.com',
+    messagingSenderId: '525963784917'
+  };
+
 const appRoutes: Routes  = [
   {path: '', component: HomeComponent },
-  {path: 'listings', component: ListingsComponent }
+  {path: 'listings', component: ListingsComponent },
+  {path: 'add-listings', component: AddListingComponent }
 ]
 
 @NgModule({
@@ -31,9 +42,10 @@ const appRoutes: Routes  = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
